4af45b1a89b341214cb358bfee9363a50c0afb84
karthikkrishnaswamy638@gmail.com <karthikkrishnaswamy638@gmail.com>
2023-05-14 14:27:32 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import aboutImage from "../assets/images/about-image.png";
+
+export const About = () => {
+
+    return (
+        <div className="bg-white">
+            <div className="p-24 grid grid-cols-2">
+                <div className="">
+                    <h2 className="text-2xl font-medium">About Us</h2>
+                    <p className="text-lg">
+                    Pizzeria is another word for a pizza restaurant or pizza parlor – a place where pizzas are made, sold and served to customers. The term pizzeria comes from the product, pizza, and the suffix, -eria, which translates from italian to “place of” in english.
+                    </p>
+                </div>
+                <div className="flex items-center justify-center">
+                    <img src={aboutImage} alt="" className="w-[400px] h-[400px] object-cover" />
+                </div>
+            </div>
+        </div>
+    )
+}
